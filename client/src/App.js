0113b77750c79f0d763617c2977e033430fb865b
LadyKerr <kadiekerr@gmail.com>
2019-08-10 13:15:00 -0400

    sidebar complete

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,10 @@ class App extends React.Component {
   render() {
     return (
       <div className="app-container">
-        <SideBar />
+        <SideBar
+          selectedNoteIndex={this.state.selectedNoteIndex}
+          notes={this.state.notes}
+        />
         <Editor />
       </div>
     );
--- client/src/sidebar/sidebar.js
+++ client/src/sidebar/sidebar.js
@@ -8,11 +8,84 @@ import SidebarItem from "../sidebarItem/sidebarItem";
 class SideBar extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      addingNote: false,
+      title: null
+    };
   }
 
+  newNoteBtnClick = () => {
+    console.log("New Note Btn Clicked");
+    this.setState({
+      addingNote: !this.state.addingNote,
+      title: null
+    });
+  };
+
+  updateTitle = text => {
+    console.log("here is the text", text);
+    this.setState({ title: text });
+  };
+
+  newNote = () => {
+    console.log(this.state);
+  };
+
+  selectNote = () => {
+    console.log("select note");
+  };
+
+  deleteNote = () => {
+    console.log("delete note");
+  };
+
   render() {
-    return <div>Hello from SideBar</div>;
+    const { notes, classes, selectedNoteIndex } = this.props;
+
+    if (notes) {
+      return (
+        <div className={classes.sidebarContainer}>
+          <Button onClick={this.newNoteBtnClick} className={classes.newNoteBtn}>
+            {this.state.addingNote ? "Cancel" : "New Note"}
+          </Button>
+          {this.state.addingNote ? (
+            <div>
+              <input
+                type="text"
+                className={classes.newNoteInput}
+                placeholder="Enter Note Title"
+                onKeyUp={e => this.updateTitle(e.target.value)}
+              />
+              <Button
+                className={classes.newNoteSubmitBtn}
+                onClick={this.newNote}
+              >
+                Submit Note
+              </Button>
+            </div>
+          ) : null}
+
+          <List>
+            {notes.map(note => {
+              return (
+                <div key={note.id}>
+                  <SidebarItem
+                    note={note}
+                    index={note.id}
+                    selectedNoteIndex={selectedNoteIndex}
+                    selectNote={this.selectNote}
+                    deleteNote={this.deleteNote}
+                  />
+                  <Divider />
+                </div>
+              );
+            })}
+          </List>
+        </div>
+      );
+    } else {
+      return <div>Add a Note!</div>;
+    }
   }
 }
 
