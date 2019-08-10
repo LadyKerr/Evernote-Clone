9f09e49406dc000e3a108cc13fd99da090a9b81b
LadyKerr <kadiekerr@gmail.com>
2019-08-10 11:57:52 -0400

    firebase notes setUp

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
 
+const firebase = require("firebase");
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      notes: null
+      notes: null,
+      selectedNoteIndex: null,
+      selectedNote: null
     };
   }
-
-  componentDidMount() {}
 
   render() {
     return (
@@ -18,6 +20,23 @@ class App extends React.Component {
       </div>
     );
   }
+
+  componentDidMount = () => {
+    firebase
+      .firestore()
+      .collection("notes") //name of the collection/table
+      .onSnapshot(serverUpdate => {
+        //automatically gets called when nites collection is updated in firebase
+        //docs is a prop of the serverUpdate object being passed in the function
+        const notes = serverUpdate.docs.map(doc => {
+          const data = doc.data(); //function which grabs data from doc
+          data["id"] = doc.id;
+          return data; //now we have an array of all the notes
+        });
+        console.log(notes);
+        this.setState({ notes: notes }); //resetting state to the array of notes we just grabbed
+      });
+  };
 }
 
 export default App;
